f45f265975ab9dea6c9c616872307205c4e8dba2
PabloLeon2005 <pabloguaman2005@gmail.com>
2026-01-08 12:13:52 -0500

    Segundo Problema

--- ejercicios/Problemas/src/paquete02/Problema2.java
+++ ejercicios/Problemas/src/paquete02/Problema2.java
@@ -5,6 +5,8 @@
  */
 package paquete02;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
@@ -15,7 +17,57 @@ public class Problema2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        String[] viviendas = obtenerViviendas();
+        
+        double[][] consumos = obtenerConsumos();
+        
+        double[] total = obtenerTotal(consumos);
+        
+        presentarReporte(viviendas, total);
     }
-    
+
+    public static String[] obtenerViviendas() {
+        Scanner entrada = new Scanner(System.in);
+        String[] viviendas = new String[10];
+        for (int i = 0; i < 10; i++) {
+            System.out.print("Nombre de la vivienda " + (i+1) + " ");
+            viviendas[i] = entrada.nextLine();
+        }
+        return viviendas;
+    }
+
+    public static double[][] obtenerConsumos() {
+        Scanner entrada = new Scanner(System.in);
+        double[][] matriz = new double[10][12];
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Registro de consumos para Vivienda " + (i+1) 
+                    + " ");
+            for (int j = 0; j < 12; j++) {
+                System.out.print("  Mes " + (j + 1) + ": ");
+                matriz[i][j] = entrada.nextDouble();
+            }
+        }
+        return matriz;
+    }
+
+    public static double[] obtenerTotal(double[][] xd) {
+        double[] totales = new double[10];
+        for (int i = 0; i < 10; i++) {
+            double sumaVivienda = 0;
+            for (int j = 0; j < 12; j++) {
+                sumaVivienda += xd[i][j];
+            }
+            totales[i] = sumaVivienda;
+        }
+        return totales;
+    }
+
+    public static void presentarReporte(String[] viviendas, double[] totales) {
+        String reporte = "REPORTE \n";
+        for (int i = 0; i < 10; i++) {
+            reporte += "Vivienda: " + viviendas[i] + "  Consumo Total: " + totales[i] + "\n";
+        }
+        System.out.println(reporte);
+    }
 }
